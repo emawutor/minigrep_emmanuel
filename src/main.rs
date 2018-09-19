d2f4654c82a7eecba354d9ff6f939572f9fcc4ec
emmanuel mawutor <emawutor@indiana.edu>
2018-09-19 16:23:23 -0700

    split run function into 3 methods

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::env; // bring the parent to scope so that it can be reused
-use std::fs::File; // to handle files
-use std::io::prelude::*; // contains useful trains for doing I/O
+// use std::fs::File; // to handle files
+//use std::io::prelude::*; // contains useful trains for doing I/O
+
+use std::process;
+use std::error::Error;
 
 // args return iterator of command line
 // env to read command line arguments
@@ -13,27 +16,43 @@ use std::io::prelude::*; // contains useful trains for doing I/O
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    println!("In file {}", filename);
+    run(config);
+}
 
-    let mut f = File::open(filename).expect("file not found");
+struct Config {
+    query: String ,
+    filename: String ,
+}
 
+impl Config {
+  fn new(args: &[String]) -> Result<Config, &'static str>  {
+      if args.len() < 3 {
+          return Err(" not enough arguments");
+      }
+      let query = args[1].clone();
+      let filename = args[2].clone();
+
+      Ok(Config { query, filename })
+  }
+}
+
+fn run(config: Config) Result<(), Box<Error>> {
+    let mut f = File::open(config.filename).expect("file not found")?;
     let mut contents = String::new();
-
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
-
     println!("With text:\n{}", contents);
 
+    Ok(())
 }
 
-fn parse_config (args: &[String]) -> (&str, &str) {
-  let query = &args[1];
-  let filename = &args[2];
 
-  (query, filename)
-}
